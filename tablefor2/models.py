6d1da9e04b31b5875a0f275b2764e6b5da8fb54a
Tiffany Qi <tiffany19qi@gmail.com>
2017-07-09 21:15:08 -0700

    modify migrations

--- tablefor2/models.py
+++ tablefor2/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, UserManager
-
 from django.db import models
-from django.utils import timezone
 
 
 class Profile(AbstractBaseUser):
@@ -15,7 +13,7 @@ class Profile(AbstractBaseUser):
     is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(default=timezone.now())
+    date_joined = models.DateTimeField()
 
     extra_saved_information = models.BooleanField(default=False)
     preferred_name = models.CharField(null=True, max_length=50)
